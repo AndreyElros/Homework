65e72a8a648e3ce0bc2e8e3eabfb75c0b3d8fa73
Elros <62696402+AndreyElros@users.noreply.github.com>
2020-04-03 16:09:36 +0300

    Update l3pr2.py

--- lesson03/l3pr2.py
+++ lesson03/l3pr2.py
@@ -1,3 +1,4 @@
+#Задание №2
 def my_func (name, surname, year, town, email, telephone):
      return ' '.join([name, surname, year, town, email, telephone])
 print(my_func(surname = 'Ivanov', telephone = '+7-905-814-22-22', year = '1993', town = 'Vladimir', name = 'Ivan', email = 'example@gmail.ru'))
