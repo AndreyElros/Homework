86d41c89ca6fa14b2d9c5f26ca37345f6bb89ba7
AndreyElros <archer.archi@bk.ru>
2020-04-03 15:23:06 +0300

    Задание 3.1

---
+++ lesson03/l3pr1.py
@@ -0,0 +1,11 @@
+def my_f(*args):
+    try:
+        n1 = int(input('Введите делимое: '))
+        n2 = int(input('Введите делитель: '))
+        a = n1 / n2
+    except ValueError:
+        return 'Ошибка значения'
+    except ZeroDivisionError:
+        return 'Неверный делитель!!! На 0 делить нельзя!'
+    return a
+print(f'Результат:  {my_f()}')
