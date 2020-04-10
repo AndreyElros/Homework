62a197750c6ff64e9d97fe49900a6a23eecb03ef
AndreyElros <archer.archi@bk.ru>
2020-04-10 14:39:01 +0300

    Задание 5.1

---
+++ lesson05/l5pr1.py
@@ -0,0 +1,13 @@
+#Задание 1
+my_list = []
+while True:
+    line = input('Введите текст: ')
+    if line == '':
+        print(my_list)
+        exit()
+    else:
+        newline = line + '\n'
+        my_list.append(newline)
+
+    with open("test.txt", "w") as file_obj:
+        file_obj.writelines(my_list)
